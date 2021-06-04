2c14ac1f0dc3eff8103b4a1ae38696e4d9b89387
varun jain <varunsanjeevjain@gmail.com>
2021-06-04 16:32:55 +0530

    Implemented via StateProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,58 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final riverpodStringProvider =
-    Provider((_) => 'Ooooh ... learning Riverpod ...... :)');
+    Provider((_) => 'Ooooh Yeah ... learning Riverpod ...... :)');
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends ConsumerWidget {
-  @override
-  Widget build(BuildContext context, ScopedReader watch) {
-    final String value = watch(riverpodStringProvider);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('RiverPod Example by @VarSVat'),
-          centerTitle: true,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+final counterProvider = StateProvider<int>((ref) => 0);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('RiverPod Example by @VarSVat'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer(builder: (context, watch, child) {
+              final providerstring = watch(riverpodStringProvider);
+              return Text(providerstring.toString());
+            }),
+            Consumer(builder: (context, watch, _) {
+              final count = watch(counterProvider).state;
+              return Text('$count');
+            }),
+          ],
         ),
-        body: Center(
-          child: Text(value),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => increment(context),
+        child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+void increment(BuildContext context) {
+  final ctrProvider = context.read(counterProvider);
+  ctrProvider.state++;
 }
